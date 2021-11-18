18d7946517a26e870293fda11014c0b697e23d44
briannaflynn <brianna.flynn@utexas.edu>
2021-11-18 11:38:14 -0600

    Adding guardrails

--- joint_space_1.py
+++ joint_space_1.py
@@ -157,6 +157,8 @@ def get_joint_space_10(femur, tibia):
 	
 	y_qa = y[quad_a][0]
 	y_qa_0 = y[quad_a_0][0]
+	print(y_qa_0)
+
 	y_qa_1 = y[quad_a_1][0]
 	
 	y_qb = y[quad_b][0]
@@ -168,54 +170,74 @@ def get_joint_space_10(femur, tibia):
 	y_half_1 = y[half_point_1][0]
 	
 	_, y = numpy_identity(femur)
+	
+	if y[quad_a_0].size > 0:
+		yfqa0 = y[quad_a_0][-1]
+	else:
+		yfqa0 = 0
+	
 	yfqa = y[quad_a][-1]
-	yfqa0 = y[quad_a_0][-1]
 	yfqa1 = y[quad_a_1][-1]
 	
 	yfha = y[half_point][-1]
 	yfha0 = y[half_point_0][-1]
 	yfha1 = y[half_point_1][-1]
 	
+	if y[quad_b_1].size > 0:
+		yfqb1 = y[quad_b_1][-1]
+	else:
+		yfqb1 = 0
+	
 	yfqb = y[quad_b][-1]
 	yfqb0 = y[quad_b_0][-1]
-	yfqb1 = y[quad_b_1][-1]
+	#yfqb1 = y[quad_b_1][-1]
 	
 	_a = abs(y_qa - yfqa)
-	_a0 = abs(y_qa_0 - yfqa0)
 	_a1 = abs(y_qa_1 - yfqa1)
-	
+
+	if yfqa0 != 0:
+		_a0 = abs(y_qa_0 - yfqa0)
+	else:
+		_a0 = _a + _a1 / 2
+		
 	_h = abs(y_half - yfha)
 	_h0 = abs(y_half_0 - yfha0)
 	_h1 = abs(y_half_1 - yfha1)
 	
+	if yfqb1 != 0:
+		_b1 = abs(y_qb_1 - yfqb1)
+	else:
+		_b1 = _b + _b0 / 2
+	
 	_b = abs(y_qb - yfqb)
 	_b0 = abs(y_qb_0 - yfqb0)
-	_b1 = abs(y_qb_1 - yfqb1)
-	
-	_average = (_a + _a0 + _a1 + _h + _h0 + _h1 + _b + _b0 + _b1) / 9
-	
-	def normalize(*args):
-		
-		ans = []
-		for a in args:
-			an = a / length
-			ans.append(an)
-			
-		return ans
-				
-		
-	norms = normalize(_a, _a0, _a1, _h, _h0, _h1, _b, _b0, _b1, _average)
-	
-	a = sum(norms[:3]) / 3
-	b = sum(norms[3:6]) / 3
-	c = sum(norms[3:6]) / 3
-	
-	if _average >= 100:
-		print("\nWARNING: Need to supply femur, then tibia, or else calculations are off!\n")
-		
-	joint_space = {"quad_1" : norms[:3], "quad_1_av": a, "quad_2" : norms[3:6], "quad_2_av": b, "quad_3" : norms[6:9], "quad_3_av": c, "average": norms[-1], "not_normal_q1-q3": [_a, _a0, _a1, _h, _h0, _h1, _b, _b0, _b1], "not_normal_average": _average, "xmax": length}
+# 	_b1 = abs(y_qb_1 - yfqb1)
+# 	
+# 	_average = (_a + _a0 + _a1 + _h + _h0 + _h1 + _b + _b0 + _b1) / 9
+# 	
+# 	def normalize(*args):
+# 		
+# 		ans = []
+# 		for a in args:
+# 			an = a / length
+# 			ans.append(an)
+# 			
+# 		return ans
+# 				
+# 		
+# 	norms = normalize(_a, _a0, _a1, _h, _h0, _h1, _b, _b0, _b1, _average)
+# 	
+# 	a = sum(norms[:3]) / 3
+# 	b = sum(norms[3:6]) / 3
+# 	c = sum(norms[3:6]) / 3
+# 	
+# 	if _average >= 100:
+# 		print("\nWARNING: Need to supply femur, then tibia, or else calculations are off!\n")
+# 		
+# 	joint_space = {"quad_1" : norms[:3], "quad_1_av": a, "quad_2" : norms[3:6], "quad_2_av": b, "quad_3" : norms[6:9], "quad_3_av": c, "average": norms[-1], "not_normal_q1-q3": [_a, _a0, _a1, _h, _h0, _h1, _b, _b0, _b1], "not_normal_average": _average, "xmax": length}
+# 
+# 	return joint_space
 
-	return joint_space
 #########################################################################################
 
 path = sys.argv[1]
@@ -250,21 +272,21 @@ def runner(files, path, df):
         
         js = get_joint_space_10(femur, tibia)
         
-        if js['average'] >= 100:
-        	f = "WARNING_" + f
-        elif js['not_normal_average'] >= 100:
-        	f = "WARNING_" + f
-        else:
-        	pass
-        
-        image_name = {'file': f}
-        
-        j = {**image_name, **js}
-        
-        df = df.append(j, True)
-        
-    return df
+#         if js['average'] >= 100:
+#         	f = "WARNING_" + f
+#         elif js['not_normal_average'] >= 100:
+#         	f = "WARNING_" + f
+#         else:
+#         	pass
+#         
+#         image_name = {'file': f}
+#         
+#         j = {**image_name, **js}
+#         
+#         df = df.append(j, True)
+#         
+#     return df
     
 data = runner(lines, path, df)
-data.to_csv(fullname, index=False)
-print(fullname, "successfully written")
+# data.to_csv(fullname, index=False)
+# print(fullname, "successfully written")
