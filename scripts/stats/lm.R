8b2813e9823eff49fbcb9d812f751e114ad4795d
briannaflynn <brianna.flynn@utexas.edu>
2022-02-08 21:18:17 -0600

    new linear regression models for betas and comparing instances, left and right:

--- scripts/stats/lm.R
+++ scripts/stats/lm.R
@@ -17,14 +17,75 @@ colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
 colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
 colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
 colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
-inst_rfta
+
 inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
-inst_data
+
 
 inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
-
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
 m1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_2, data = inst_rfta)
 summary(m1)
+
+m1.0 <- lm(left_quad_all_av_norm_3 ~ left_quad_all_av_norm_2 * left_x_2, data = inst_lfta)
+summary(m1.0)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_space_zero_norm_by_height.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_space_zero_norm_by_height.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("joint_space_zero_norm_by_height.csv")
+m1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+print("joint_space_zero_norm_by_height.csv")
+m1.0 <- lm(left_quad_all_av_norm_3 ~ left_quad_all_av_norm_2 * left_x_2, data = inst_lfta)
+summary(m1.0)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_space_xygroup_zero_ratio_norm_by_height.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_space_xygroup_zero_ratio_norm_by_height.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("joint_space_xygroup_zero_ratio_norm_by_height.csv")
+m1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+print("joint_space_xygroup_zero_ratio_norm_by_height.csv")
+m1.0 <- lm(left_quad_all_av_norm_3 ~ left_quad_all_av_norm_2 * left_x_2, data = inst_lfta)
+summary(m1.0)
 
 m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
 summary(m2)
@@ -42,15 +103,19 @@ colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
 colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
 colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
 colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
-inst_rfta
+
 inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
-inst_data
+
 
 inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
 inst_data$right_x_2 <- factor(inst_data$right_x_2)
-
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("joint_space_xygroup_zero_norm_by_height.csv")
 m1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_2, data = inst_rfta)
 summary(m1)
+print("joint_space_xygroup_zero_norm_by_height.csv")
+m1.0 <- lm(left_quad_all_av_norm_3 ~ left_quad_all_av_norm_2 * left_x_2, data = inst_lfta)
+summary(m1.0)
 
 m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
 summary(m2)
@@ -78,7 +143,7 @@ inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
 inst_data
 
 inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
-
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
 m1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_2, data = inst_rfta)
 summary(m1)
 
@@ -90,3 +155,120 @@ summary(m1.1)
 
 m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
 summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_area.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_area.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+inst_rfta
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+inst_data
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+m1 <- lm(right_quad_all_area_norm_3 ~ right_quad_all_area_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+m10 <- lm(left_quad_all_area_norm_3 ~ left_quad_all_area_norm_2 * left_x_2, data = inst_lfta)
+summary(m10)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_zero_norm_by_DXA_area.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_zero_norm_by_DXA_area.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+inst_rfta
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+inst_data
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("inst_joint_zero_norm_by_DXA_area.csv")
+m1 <- lm(right_quad_all_area_norm_3 ~ right_quad_all_area_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+print("inst_joint_zero_norm_by_DXA_area.csv")
+m10 <- lm(left_quad_all_area_norm_3 ~ left_quad_all_area_norm_2 * left_x_2, data = inst_lfta)
+summary(m10)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_area_AB_groups_zero_norm_by_DXA_area.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_area_AB_groups_zero_norm_by_DXA_area.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+inst_rfta
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+inst_data
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("inst_joint_area_AB_groups_zero_norm_by_DXA_area.csv")
+m1 <- lm(right_quad_all_area_norm_3 ~ right_quad_all_area_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+print("inst_joint_area_AB_groups_zero_norm_by_DXA_area.csv")
+m10 <- lm(left_quad_all_area_norm_3 ~ left_quad_all_area_norm_2 * left_x_2, data = inst_lfta)
+summary(m10)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+inst_rfta = read.csv('./replicates/data/right/inst_right_joint_area_AB_groups_zero_ratio_norm_by_DXA_area.csv') 
+inst_lfta = read.csv('./replicates/data/left/inst_left_joint_area_AB_groups_zero_ratio_norm_by_DXA_area.csv')
+
+colnames(inst_rfta) <- paste("right", colnames(inst_rfta), sep = "_")
+colnames(inst_lfta) <- paste("left", colnames(inst_lfta), sep = "_")
+colnames(inst_rfta)[which(names(inst_rfta) == "right_eid")] <- "eid"
+colnames(inst_lfta)[which(names(inst_lfta) == "left_eid")] <- "eid"
+inst_rfta
+inst_data <- merge(inst_rfta, inst_lfta, by = "eid")
+inst_data
+
+inst_rfta$right_x_2 <- factor(inst_rfta$right_x_2)
+inst_lfta$left_x_2 <- factor(inst_lfta$left_x_2)
+print("ratio_norm_by_DXA_area")
+m1 <- lm(right_quad_all_area_norm_3 ~ right_quad_all_area_norm_2 * right_x_2, data = inst_rfta)
+summary(m1)
+print("ratio_norm_by_DXA_area")
+m10 <- lm(left_quad_all_area_norm_3 ~ left_quad_all_area_norm_2 * left_x_2, data = inst_lfta)
+summary(m10)
+
+m2 <- lm(right_quad_all_av_norm_2 ~ left_quad_all_av_norm_2 * right_x_2, data = inst_data)
+summary(m2)
+
+m1.1 <- lm(right_quad_all_av_norm_3 ~ right_quad_all_av_norm_2 * right_x_3, data = inst_rfta)
+summary(m1.1)
+
+m2.1 <- lm(right_quad_all_av_norm_2 ~ 0 + left_quad_all_av_norm_2, data = inst_data)
+summary(m2.1)
+
+
+
--- scripts/stats/regr.R
+++ scripts/stats/regr.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
 library(broom)
-
+# Joint Space
 root = "../../data/predictions/cleaned/1pct_x_z3_y_iqr/"
 right = read.csv(paste0(root, "right/right_joint_space_raw.csv"))
 left = read.csv(paste0(root, "left/left_joint_space_raw.csv"))
@@ -9,6 +9,11 @@ rfta = read.csv(paste0(root, "right/right_joint_space_norm_by_ft_average.csv"))
 rfta <- rfta[, c("file", "fem_tib_average")]
 lfta = read.csv(paste0(root, "left/left_joint_space_norm_by_ft_average.csv"))
 lfta <- lfta[, c("file", "fem_tib_average")]
+# Joint Area
+root = 'replicates/data/'
+right = read.csv(paste0(root, 'right/right_joint_area.csv'))
+left = read.csv(paste0(root, 'left/left_joint_area.csv'))
+
 # rh = read.csv("right_standing_height.csv")
 # lh = read.csv("left_standing_height.csv")
 # rh = subset(rh, select=c(file, height))
@@ -154,3 +159,81 @@ autoregr_zero(left_groups, lu, "left_xy_regr")
 
 write.csv(right_groups, "right_joint_space_xygroup_raw.csv", row.names = FALSE)
 write.csv(left_groups, "left_joint_space_xygroup_raw.csv", row.names = FALSE)
+
+# area, x values
+autoregr_zero <- function(data, uniques, namestr) {
+  
+  for (xval in uniques) {
+    df <- subset(data, x == xval)
+    modl <- lm(all_area_average ~ 0 + combined_area_i2, data=df)
+    xv <- as.character(xval)
+    fname <- paste0(namestr, "_", xv, "_zero_area.csv")
+    print(fname)
+    tidy_modl <- tidy(modl)
+    print(summary(modl))
+    
+    write.csv(tidy_modl, fname)
+    
+  }
+}
+
+colnames(right)
+ca <- read.csv('../../data/combined_leg_area.csv')
+right <- merge(ca, right, by = "eid")
+left <- merge(ca, left, by = "eid")
+
+ru <- unique(right$x)
+lu <- unique(left$x)
+
+autoregr_zero(right, ru, "r")
+autoregr_zero(left, lu, "l")
+
+
+# area, AB group
+autoregr_zero <- function(data, uniques, namestr) {
+  
+  for (xval in uniques) {
+    df <- subset(data, AB_groups == xval)
+    modl <- lm(all_area_average ~ 0 + combined_area_i2, data=df)
+    xv <- as.character(xval)
+    fname <- paste0(namestr, "_", xv, "_zero_area.csv")
+    print(fname)
+    tidy_modl <- tidy(modl)
+    print(summary(modl))
+    
+    write.csv(tidy_modl, fname)
+    
+  }
+}
+
+colnames(right)
+ca <- read.csv('../../data/combined_leg_area.csv')
+right <- merge(ca, right, by = "eid")
+left <- merge(ca, left, by = "eid")
+
+rAB <- read.csv(paste0(root, 'right/right_AB.csv'))
+lAB <- read.csv(paste0(root, 'left/left_AB.csv'))
+
+
+rAB <- rAB[, c('file', 'AB_groups')]
+lAB <- lAB[, c('file', 'AB_groups')]
+
+right <- merge(rAB, right, on = "file")
+left <- merge(lAB, left, on = "file")
+ru <- unique(right$AB_groups)
+lu <- unique(left$AB_groups)
+
+autoregr_zero(right, ru, "r")
+autoregr_zero(left, lu, "l")
+colnames(right)
+
+rxy <- read.csv('right_joint_space_xygroup_raw.csv')
+lxy <- read.csv('left_joint_space_xygroup_raw.csv')
+
+rxy <- rxy[, c('file', 'xy_group')]
+lxy <- lxy[, c('file', 'xy_group')]
+right <- merge(rxy, right, on = "file")
+left <- merge(lxy, left, on = "file")
+
+write.csv(right, "replicates/data/right_joint_area_AB_groups.csv", row.names = FALSE)
+write.csv(left, "replicates/data/left_joint_area_AB_groups.csv", row.names = FALSE)
